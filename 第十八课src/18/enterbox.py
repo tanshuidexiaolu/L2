3417458b17bfa0e5e839949a00dca540ceca582f
tanshuidexiaolu <xiaoluchuping@163.com>
2020-06-24 21:12:26 +0800

    更新

--- 第十八课src/18/enterbox.py
+++ 第十八课src/18/enterbox.py
@@ -1,5 +1,7 @@
 import easygui
-city = easygui.enterbox('请输入要查询的中国城市',title='city')
-#请在下方书写你的代码
-with open('message.txt','w',encoding='utf-8') as f:
-    f.write(city)
+
+city = easygui.enterbox('请输入要查询的中国城市', title='city')
+# 请在下方书写你的代码
+with open('message.txt', 'w', encoding='utf-8') as f:
+    f.write(city + '\n')
+
--- 第十八课src/18/index.py
+++ 第十八课src/18/index.py
@@ -2,13 +2,16 @@ import pygame
 from sys import exit
 import time
 import requests
-from subprocess import Popen,PIPE
+from subprocess import Popen, PIPE
+
+
 # 绘制文本方法
 def fillText(content, size, pos):
     fontColor = (255, 255, 255)
     fontObj = pygame.font.Font("font/simhei.ttf", size)  # 通过字体文件获得字体对象
     font = fontObj.render(content, True, fontColor)  # 配置要显示的文字
     canvas.blit(font, pos)  # 绘制字
+
 
 def showDetail():
     global city
@@ -24,6 +27,7 @@ def showDetail():
     fillText(high, 25, (100, 350))
     fillText(low, 25, (325, 350))
 
+
 def getToday(cityName):
     url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + cityName
     response = requests.get(url)
@@ -32,7 +36,7 @@ def getToday(cityName):
     if weatherDict['desc'] == 'OK':
         print('你输入的城市是正确的')
         # 获取城市名
-        global city, wendu,month,forecast
+        global city, wendu, month, forecast
         city = weatherDict['data']['city']
         print('城市名称', city)
         # 获取当前温度
@@ -53,7 +57,8 @@ def getToday(cityName):
     else:
         print('你输入的城市是错误的')
 
-getToday('三亚')
+
+getToday('西安')
 
 # pygame初始化
 pygame.init()
@@ -77,10 +82,13 @@ while True:
                 # 获取鼠标坐标位置
                 pos = pygame.mouse.get_pos()
                 if 524 <= pos[0] <= 709 and 448 <= pos[1] <= 509:
-                    p = Popen('python enterbox.py',shell=True,stdout=PIPE)
+                    p = Popen('enterbox.exe', shell=True, stdout=PIPE)
                     print(p.stdout.readline())
-                    #请在下方书写你的代码
-                    
+                    # 请在下方书写你的代码
+                    # open --> open('文件名'，'w')写入
+                    #          open('文件名')读取
+                    #          open('文件名',a)写入，但是往末尾a = append
+                    with open('message.txt',encoding='utf-8') as f:
+                        msg = f.read()
+                        print(msg)
     pygame.display.update()
-    
-    
