c75f61dfc2c18f5b0d5897e55b12d94b057dfc27
tanshuidexiaolu <xiaoluchuping@163.com>
2020-06-27 15:23:27 +0800

    更新

--- 第十九课src/19/index.py
+++ 第十九课src/19/index.py
@@ -1,7 +1,10 @@
+# coding:utf-8
 import pygame
+from pygame.locals import *
 from sys import exit
 import requests
-from subprocess import Popen,PIPE
+from subprocess import Popen, PIPE
+
 
 # 绘制文本方法
 def fillText(content, size, pos):
@@ -10,13 +13,15 @@ def fillText(content, size, pos):
     font = fontObj.render(content, True, fontColor)  # 配置要显示的文字
     canvas.blit(font, pos)  # 绘制字
 
+
 def showDetail():
-    global city, wendu,ganmao,type
+    global city, wendu, ganmao, type
     fillText(city, 45, (350, 185))
     fillText(wendu, 30, (300, 285))
     fillText(type, 35, (440, 280))
     fillText(ganmao[0:20], 18, (200, 460))
     fillText(ganmao[20:], 18, (200, 490))
+
 
 # pygame初始化
 pygame.init()
@@ -24,12 +29,28 @@ global canvas
 canvas = pygame.display.set_mode((1050, 660))
 pygame.display.set_caption('天气助手')
 
-#请在下方书写你的代码℃
 
-
+# 请在下方书写你的代码℃
+def getToday(cityName):
+    url = "http://wthrcdn.etouch.cn/weather_mini?city=" + cityName
+    response = requests.get(url)
+    weathDict = response.json()
+    # 声明全局变量城市，温度，感冒，天气类型
+    global city, wendu, ganmao, type
+    city = weathDict['data']['city']
+    wendu = weathDict['data']['wendu']
+    ganmao = weathDict['data']['ganmao']
+    type = weathDict['data']['forecast'][0]['type']
+getToday("西安")
+bg = pygame.image.load("images/bg.png")
+canvas.blit(bg,(0,0))
+showDetail()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-        
+        if event.type == MOUSEBUTTONDOWN and event.button == 1:
+            if 680 < event.pos[0] < 934 and 63 < event.pos[1] < 134:
+                print("yeah")
+
     pygame.display.update()
